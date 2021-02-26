c3acdc8bc11eed854a538213aad451555c55dc0d
jessica you <jessicaxytan@gmail.com>
2021-02-25 20:03:59 -0500

    commit tree 798d79b0d3e6838c121e0e762e6f887f549788d4

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+
+
+def line(number)
+    if number.length == 0
+        puts "The line is currently empty."
+    else
+        line_wait = "The line is currently:"
+        number.each.with_index(1) do | individual, place_in_line |
+            line_wait << " #{place_in_line}. #{individual}"
+        end
+        puts line_wait
+    end
+
+end
+
+
+def take_a_number(number, name)
+    number << name
+    puts "Welcome, #{name}. You are number #{number.length} in line."
+
+end
+
+
+def now_serving(wait)
+    if wait.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        serving = wait.shift
+        puts "Currently serving #{serving}."
+    end
+end
